d31991727dd07220a158000248a4b5fef8f196c3
hasib2019 <hasib.9437.hu@gmail.com>
2023-12-07 01:00:00 +0600

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,6 @@ use App\Models\User;
 use App\Notifications\ShopProductNotification;
 use Carbon\Carbon;
 use Combinations;
-use CoreComponentRepository;
 use Artisan;
 use Cache;
 use Str;
@@ -62,8 +61,6 @@ class ProductController extends Controller
      */
     public function admin_products(Request $request)
     {
-        CoreComponentRepository::instantiateShopRepository();
-
         $type = 'In House';
         $col_name = null;
         $query = null;
@@ -174,8 +171,6 @@ class ProductController extends Controller
      */
     public function create()
     {
-        CoreComponentRepository::initializeCache();
-
         $categories = Category::where('parent_id', 0)
             ->where('digital', 0)
             ->with('childrenCategories')
@@ -263,8 +258,6 @@ class ProductController extends Controller
      */
     public function admin_product_edit(Request $request, $id)
     {
-        CoreComponentRepository::initializeCache();
-
         $product = Product::findOrFail($id);
         if ($product->digital == 1) {
             return redirect('admin/digitalproducts/' . $id . '/edit');
